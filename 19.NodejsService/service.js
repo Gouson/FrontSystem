f42f9eb11fd25de808e0b2f3ecd46a68f475e211
Gouson <gouson@qq.com>
2020-10-26 17:49:38 +0800

    'node'

--- 19.NodejsService/service.js
+++ 19.NodejsService/service.js
@@ -24,17 +24,17 @@ var server = http.createServer(function(request, response){
   if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`二哈`)
+    response.write(`<link rel="stylesheet" href="./style"><h1>index主页</h1>`)
     response.end()
-  } else if(path === '/x'){
+  } else if(path === '/style'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/css;charset=utf-8')
-    response.write(`body{color: red;}`)
+    response.write(`h1{color: red}`)
     response.end()
-  } else {
+  }else {
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`你输入的路径不存在对应的内容`)
+    response.write(`<div>你访问的页面不存在</div>`)
     response.end()
   }
 
