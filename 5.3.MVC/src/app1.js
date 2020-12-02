2b3532d0a0b732017e989d279f53442b7be40be0
Gouson <gouson@qq.com>
2020-12-02 17:33:53 +0800

    vue

--- 5.3.MVC/src/app1.js
+++ 5.3.MVC/src/app1.js
@@ -2,15 +2,14 @@ import $ from "jquery";
 import "./app1.css";
 import Model from "./base/Model.js";
 import View from "./base/View.js"
-const eventBus = $(window);
 //数据相关 都放到m
 const m = new Model({
     data: {
-        n: parseInt(localStorage.getItem("n"))
+        n: parseFloat(localStorage.getItem("n"))
     },
     update(data) {
         Object.assign(m.data, data);
-        eventBus.trigger("m:updated");
+        m.trigger("m:updated");
         localStorage.setItem("n", m.data.n);
     }
 });
@@ -32,9 +31,11 @@ const v = {
         `,
 }
 //其他相关都放到c
-const view = {
-    el: null,
-    html: `
+const init = (el) => {
+    new View({
+        el: el,
+        data: m.data,
+        html: `
         <div>
             <div class="output">
                 <span id="number">{{n}}</span>
@@ -47,57 +48,41 @@ const view = {
             </div>
         </div>
         `,
-    init(container) {
-        view.el = $(container);
-        view.render(m.data.n); //view= render(data)
-
-        view.autoBindEvents();
-        eventBus.on("m:updated", () => {
-            view.render(m.data.n);
-        });
-    },
-    render(n) {
-        if (view.el.children.length !== 0) view.el.empty();
-        $(view.html.replace("{{n}}", n)).appendTo(view.el);
-    },
-    events: {
-        "click #add1": "add",
-        "click #minus1": "minus",
-        "click #mul2": "mul",
-        "click #divide2": "divide",
-    },
-    add() {
-        m.data.n += 1;
-        m.update({
-            n: m.data.n + 1,
-        });
-    },
-    minus() {
-        m.update({
-            n: m.data.n - 1,
-        });
-    },
-    mul() {
-        m.update({
-            n: m.data.n * 2,
-        });
-    },
-    divide() {
-        m.update({
-            n: m.data.n / 2,
-        });
-    },
-    autoBindEvents() {
-        for (let key in view.events) {
-            const spaceIndex = key.indexOf(" ");
-            const part1 = key.slice(0, spaceIndex);
-            const part2 = key.slice(spaceIndex + 1);
-            const value = view[view.events[key]];
-            view.el.on(part1, part2, value);
+        render(data) {
+            const n = data.n
+            if (this.el.children.length !== 0) this.el.empty();
+            $(this.html.replace("{{n}}", n)).appendTo(this.el);
+        },
+        events: {
+            "click #add1": "add",
+            "click #minus1": "minus",
+            "click #mul2": "mul",
+            "click #divide2": "divide",
+        },
+        add() {
+            m.data.n += 1;
+            m.update({
+                n: m.data.n + 1,
+            });
+        },
+        minus() {
+            m.update({
+                n: m.data.n - 1,
+            });
+        },
+        mul() {
+            m.update({
+                n: m.data.n * 2,
+            });
+        },
+        divide() {
+            m.update({
+                n: m.data.n / 2,
+            });
         }
-    },
-};
+    })
+}
 
 // 初始化Html
 
-export default view;
+export default init;
--- 5.3.MVC/src/app2.js
+++ 5.3.MVC/src/app2.js
@@ -1,7 +1,9 @@
 import $ from "jquery"
 import './app2.css'
 import Model from "./base/Model.js"
-const eventBus = $(window)
+import View from "./base/View"
+import EventBus from "./base/EventBus"
+const eventBus = new EventBus()
 const localKey = 'app2.index'
 const m = new Model({
     data: {
@@ -16,51 +18,42 @@ const m = new Model({
 const v = {
 
 }
-const c = {
-    el: null,
-    html: (index) => {
-        return `
-    <div>
-    <ol class="tab-bar">
-        <li class=${index===0?'selected':'none'}    data-index='0'>1</li>
-        <li class=${index===1?'selected':'none'}     data-index='1'>2</li>
-    </ol>
-    <ol class="tab-content">
-        <li class=${index===0?'active':' '}>内容1</li>
-        <li class=${index===1?'active':' '}>内容2</li>
-    </ol>
-    </div>
-    `
-    },
-    render(index) {
-        if (c.el.children.length !== 0) c.el.empty()
-        $(c.html(index)).appendTo(c.el)
-    },
-    init(container) {
-        c.el = $(container)
-        c.render(m.data.index) //view= render(data)
-        c.autoBindEvents()
-        eventBus.on('m:updated', () => {
-            c.render(m.data.index)
-        })
-    },
-    events: {
-        'click .tab-bar li': 'x'
-    },
-    x(e) {
-        const index = parseInt(e.currentTarget.dataset.index)
-        m.update({
-            index
-        })
-    },
-    autoBindEvents() {
-        for (let key in c.events) {
-            const spaceIndex = key.indexOf(' ')
-            const part1 = key.slice(0, spaceIndex)
-            const part2 = key.slice(spaceIndex + 1)
-            const value = c[c.events[key]]
-            c.el.on(part1, part2, value)
+
+
+const init = (el) => {
+    new View({
+        el: el,
+        data: m.data,
+        eventBus: eventBus,
+        html: (index) => {
+            return `
+        <div>
+        <ol class="tab-bar">
+            <li class=${index===0?'selected':'none'}    data-index='0'>1</li>
+            <li class=${index===1?'selected':'none'}     data-index='1'>2</li>
+        </ol>
+        <ol class="tab-content">
+            <li class=${index===0?'active':' '}>内容1</li>
+            <li class=${index===1?'active':' '}>内容2</li>
+        </ol>
+        </div>
+        `
+        },
+        render(data) {
+            const index = data.index
+            if (this.el.children.length !== 0) this.el.empty()
+            $(this.html(index)).appendTo(this.el)
+        },
+
+        events: {
+            'click .tab-bar li': 'x'
+        },
+        x(e) {
+            const index = parseInt(e.currentTarget.dataset.index)
+            m.update({
+                index
+            })
         }
-    }
+    })
 }
-export default c
+export default init
