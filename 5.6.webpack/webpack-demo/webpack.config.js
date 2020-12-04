218edd6dd9f893f1ee50b4f1acaed5912a12e6a9
Gouson <gouson@qq.com>
2020-12-04 11:15:58 +0800

    webpack

--- 5.6.webpack/webpack-demo/webpack.config.js
+++ 5.6.webpack/webpack-demo/webpack.config.js
@@ -2,9 +2,8 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
-    // entry: './src/index.js',
-    // output: {
-    //     filename: 'main.js',
-    //     path: path.resolve(__dirname, 'dist'),
-    // },
+    entry: './src/index.js',
+    output: {
+        filename: '[name].[contenthash].js'
+    }
 };
