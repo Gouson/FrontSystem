f0b311e4a649f7465c0d13e55366b0bc4f620245
Gouson <gouson@qq.com>
2020-12-04 14:32:12 +0800

    webpack css

--- 5.6.webpack/webpack-demo/webpack.config.js
+++ 5.6.webpack/webpack-demo/webpack.config.js
@@ -1,9 +1,21 @@
 const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'production',
     entry: './src/index.js',
     output: {
         filename: '[name].[contenthash].js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'Gouson',
+        filename: 'assets/gousonIndex.html',
+        template: 'src/assets/template.html'
+    })],
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        }, ]
     }
 };
