b4805c674741457689017df1366a6ab773c37c97
Gouson <gouson@qq.com>
2021-01-29 18:08:37 +0800

    nodejs 数据库

--- 9.2.NodeJSStaticServe/servers/index2.ts
+++ 9.2.NodeJSStaticServe/servers/index2.ts
@@ -1,29 +1,50 @@
 import * as http from 'http';
 import { IncomingMessage, ServerResponse } from 'http';
+import * as fs from 'fs';
+import * as p from 'path';
+import * as url from 'url';
 const server = http.createServer()
-server.on('request', (request: IncomingMessage, respnose: ServerResponse) => {
-    console.log(request.method)
-    console.log(request.url)
-    console.log(request.headers)
+let catchAge = 3600 * 24 * 365
+const publicDir = p.resolve(__dirname, 'public')
+server.on('request', (request: IncomingMessage, response: ServerResponse) => {
+    const { method, url: path, headers } = request
+    if (method !== 'GET') {
+        response.statusCode = 405
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.end('不支持非GET请求')
+        return
+    }
+    const { pathname, search } = url.parse(path)
+    console.log(pathname, search)
+    // response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    let filename = pathname.substr(1)
+    if (filename === '') { filename = 'index.html' }
+    fs.readFile(p.resolve(publicDir, filename), (error, data) => {
+        if (error) {
+            if (error.errno === -4058) {
+                response.statusCode = 404
+                response.setHeader('Content-Type', 'text/html;charset=utf-8')
+                fs.readFile(p.resolve(publicDir, '404.html'), (error, data) => {
+                    response.end(data)
+                })
 
-    const array = []
-    request.on('data', (chunk: Buffer) => {
-        array.push(chunk)
+            } else if (error.errno === -4068) {
+                response.statusCode = 403
+                response.setHeader('Content-Type', 'text/html;charset=utf-8')
+                response.end('无权限访问目录')
+            } else {
+                response.statusCode = 500
+                response.setHeader('Content-Type', 'text/html;charset=utf-8')
+                response.end('服务器有问题')
+            }
+
+        } else {
+            //返回文件内容
+            response.setHeader('Cache-Control', `public,max-age=${catchAge}`)
+            response.end(data)
+        }
+
     })
-    request.on('end', (chunk) => {
-        const body = Buffer.concat(array).toString()
-        console.log(body)
-
-        respnose.statusCode = 400
-        respnose.setHeader('x-gouson', 'iammmm')
-        // respnose.setHeader('Content-type', 'image/png')
-
-        respnose.write('1\n')
-        respnose.write('2\n')
-        respnose.write('3\n')
-        respnose.end()
-    })
-
 })
 
 server.listen(8888)
